d6e634260c71beb898ab060678da3f2580383ed5
Iage von Linsingen <iagevlinsingen@gmail.com>
2025-06-27 07:24:57 -0300

    Arquivo do trabalho e estrutura geral do programa

---
+++ trabalho.asm
@@ -0,0 +1,19 @@
+;TRABALHO INTEL
+
+.model small
+.stack 100h 
+
+.data ; variáveis do programa 
+nCol dw ?
+
+
+.code 
+    .startup ; começo do programa
+
+
+
+
+
+    .exit ; fim do programa
+
+end 
